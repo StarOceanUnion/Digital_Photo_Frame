473ad2c5d8375078177700fa86cf4079949629f9
eric <2680773424@qq.com>
2023-04-27 01:34:23 +0800

    update

--- include/fonts_manager.h
+++ include/fonts_manager.h
@@ -5,6 +5,13 @@
 #ifndef _FONTS_MANAGER_H
 #define _FONTS_MANAGER_H
 
+typedef struct FontOpr{
+    char *name;
+    int (*FontInit)(char *pcFont, unsigned int dwFontSize);    //set a function to init the font's information
+    int (*GetFontBitmap)(unsigned int dwCode, PT_FontBitMap ptFontBitMap);
+    struct FontOpr *ptNext;
+}T_FontOpr, *PT_FontOpr;
+
 
 
 #endif //_FONTS_MANAGER_H
